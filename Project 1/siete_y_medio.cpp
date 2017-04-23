5c917ba98c568d18f2edf9878c0852a5cde11cf6
Ethan Waldman <ewaldman4@yahoo.com>
2017-04-22 21:40:33 -0700

    Began main file

--- Project 1/siete_y_medio.cpp
+++ Project 1/siete_y_medio.cpp
@@ -21,4 +21,15 @@ const int DEALER_MONEY = 900;
 int main() {
 	Player Dealer(DEALER_MONEY);
 	Player User(USER_MONEY);
+	cout << "Let's begin our game of Siete y Medio.\nYour current wallet is " << USER_MONEY << "\n";
+	int betAmount = 0;
+	do {
+		cout << "   Enter bet amount: ";
+		cin >> betAmount;
+		if (betAmount > USER_MONEY)
+		{
+			cout << "You're trying to cheat, aren't you?\nYou can only bet as much as you have!\n   Enter bet amount: ";
+			cin >> betAmount;
+		}
+	} while (false);
 }
