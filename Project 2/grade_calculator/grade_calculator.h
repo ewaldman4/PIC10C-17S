f84ad82ab3bd721de8010a1fcd64c6a0a70a9129
ewaldman4 <ewaldman4@yahoo.com>
2017-05-26 14:15:55 -0700

    Added comments to the header file

--- Project 2/grade_calculator/grade_calculator.h
+++ Project 2/grade_calculator/grade_calculator.h
@@ -12,15 +12,23 @@ class Grade_Calculator : public QMainWindow
     Q_OBJECT
 
 public:
+    //the explicit constructor
     explicit Grade_Calculator(QWidget *parent = 0);
+    //the destructor
     ~Grade_Calculator();
+    //this function returns a double cooresponding to the PIC10C overall grade
     double calculatePIC10CGrade();
+    //this function returns a double cooresponding to the Math131BH overall grade
     double calculateMath131BHGrade();
 
 private slots:
+    //this slot changes the grading scheme to PIC10C when the radio button labeled as such is pressed
     void radioButtonClicked();
+    //this slot changes the grading scheme to Math131BH when the radio button labeled as such is pressed
     void radioButton2Clicked();
+    //this slot displays the grade when the push button "Submit" is clicked
     void submitClicked();
+    //this slot resets all values to 0 when the push button "Reset" is clicked
     void resetClicked();
 
 private:
