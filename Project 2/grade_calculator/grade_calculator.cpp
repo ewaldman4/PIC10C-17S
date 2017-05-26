db1f1cc1d1380402425d55b28e03a374cd50e1e3
ewaldman4 <ewaldman4@yahoo.com>
2017-05-25 17:03:53 -0700

    Update grade_calculator.cpp
    Really just changed everything to the point of it working.

--- Project 2/grade_calculator/grade_calculator.cpp
+++ Project 2/grade_calculator/grade_calculator.cpp
@@ -8,6 +8,32 @@ Grade_Calculator::Grade_Calculator(QWidget *parent) :
     ui(new Ui::Grade_Calculator)
 {
     ui->setupUi(this);
+    QObject::connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(submitClicked()));
+    QObject::connect(ui->radioButton, SIGNAL(clicked()), this, SLOT(radioButtonClicked()));
+    QObject::connect(ui->radioButton_2, SIGNAL(clicked()), this, SLOT(radioButton2Clicked()));
+
+    ui->HW1_Received_Score->setMaximum(100);
+    ui->HW1_Total_Score->setMaximum(100);
+    ui->HW2_Received_Score->setMaximum(100);
+    ui->HW2_Total_Score->setMaximum(100);
+    ui->HW3_Received_Score->setMaximum(100);
+    ui->HW3_Total_Score->setMaximum(100);
+    ui->HW4_Received_Score->setMaximum(100);
+    ui->HW4_Total_Score->setMaximum(100);
+    ui->HW5_Received_Score->setMaximum(100);
+    ui->HW5_Total_Score->setMaximum(100);
+    ui->HW6_Received_Score->setMaximum(100);
+    ui->HW6_Total_Score->setMaximum(100);
+    ui->HW7_Received_Score->setMaximum(100);
+    ui->HW7_Total_Score->setMaximum(100);
+    ui->HW8_Received_Score->setMaximum(100);
+    ui->HW8_Total_Score->setMaximum(100);
+    ui->Midterm_Received_Score->setMaximum(100);
+    ui->Midterm_Total_Score->setMaximum(100);
+    ui->FinalExam_Received_Score->setMaximum(100);
+    ui->FinalExam_Total_Score->setMaximum(100);
+    ui->FinalProject_Received_Score->setMaximum(100);
+    ui->FinalProject_Total_Score->setMaximum(100);
 }
 
 Grade_Calculator::~Grade_Calculator()
@@ -31,6 +57,11 @@ double Grade_Calculator:: calculatePIC10CGrade(){
     double overallGradeScheme1 = (.15 * averageHomeworkScore + .25 * midtermScore + .3 * finalExamScore + .3 * finalProjectScore) * 100;
     double overallGradeScheme2 = (.15 * averageHomeworkScore + .5 * finalExamScore + .35 * finalProjectScore) * 100;
 
+    if(std::isnan(overallGradeScheme1)||std::isnan(overallGradeScheme2))
+    {
+        return -1;
+    }
+
     if(overallGradeScheme1 >= overallGradeScheme2)
     {
         return overallGradeScheme1;
@@ -41,17 +72,47 @@ double Grade_Calculator:: calculatePIC10CGrade(){
     }
 }
 
+
+double Grade_Calculator:: calculateMath131BHGrade(){
+    double hw1Score = (ui->HW1_Received_Score->text()).toDouble()/(ui->HW1_Total_Score->text()).toDouble();
+    double hw2Score = (ui->HW2_Received_Score->text()).toDouble()/(ui->HW2_Total_Score->text()).toDouble();
+    double hw3Score = (ui->HW3_Received_Score->text()).toDouble()/(ui->HW3_Total_Score->text()).toDouble();
+    double hw4Score = (ui->HW4_Received_Score->text()).toDouble()/(ui->HW4_Total_Score->text()).toDouble();
+    double hw5Score = (ui->HW5_Received_Score->text()).toDouble()/(ui->HW5_Total_Score->text()).toDouble();
+    double hw6Score = (ui->HW6_Received_Score->text()).toDouble()/(ui->HW6_Total_Score->text()).toDouble();
+    double hw7Score = (ui->HW7_Received_Score->text()).toDouble()/(ui->HW7_Total_Score->text()).toDouble();
+    double hw8Score = (ui->HW8_Received_Score->text()).toDouble()/(ui->HW8_Total_Score->text()).toDouble();
+    int numberOfHWAssignments = 8;
+    double averageHomeworkScore = (hw1Score + hw2Score + hw3Score + hw4Score + hw5Score + hw6Score + hw7Score + hw8Score)/numberOfHWAssignments;
+
+    double midtermScore = (ui->Midterm_Received_Score->text()).toDouble()/(ui->Midterm_Total_Score->text()).toDouble();
+
+    double finalExamScore = (ui->FinalExam_Received_Score->text()).toDouble()/(ui->FinalExam_Total_Score->text()).toDouble();
+
+    double overallGradeScheme = (.4 * averageHomeworkScore + .2 * midtermScore + .4 * finalExamScore) * 100;
+
+
+    if(std::isnan(overallGradeScheme))
+    {
+        return -1;
+    }
+
+    return overallGradeScheme;
+}
+
 void Grade_Calculator:: radioButtonClicked(){
     typePIC10C = true;
+    typeMath131BH = false;
 }
 
 void Grade_Calculator:: radioButton2Clicked(){
     typeMath131BH = true;
+    typePIC10C = false;
 }
 
 void Grade_Calculator:: submitClicked(){
-    double grade = 0;
-    if(typePic10C == true)
+    double grade = -2;
+    if(typePIC10C == true)
     {
         grade = calculatePIC10CGrade();
     }
@@ -59,5 +120,16 @@ void Grade_Calculator:: submitClicked(){
     {
         grade = calculateMath131BHGrade();
     }
-    ui->lineEdit->setText(grade + "%");
+    if(grade == -1)
+    {
+       ui->lineEdit->setText("Invalid Input");
+    }
+    else if(grade == -2)
+    {
+      ui->lineEdit->setText("Please choose a class scheme.");
+    }
+    else
+    {
+      ui->lineEdit->setText(QString::number(grade) + "%");
+    }
 }
