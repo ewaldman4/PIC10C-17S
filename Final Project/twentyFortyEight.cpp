345cffa92a8f71d38ce47bf235dd79d075645657
ewaldman4 <ewaldman4@yahoo.com>
2017-06-11 15:26:18 -0700

    Completed random placement function

--- Final Project/twentyFortyEight.cpp
+++ Final Project/twentyFortyEight.cpp
@@ -51,16 +51,20 @@ void TwentyFortyEight::changeColor(int verticalPosition, int horizontalPosition)
 {
     switch(valuesOfGrid[verticalPosition][horizontalPosition]->text().toInt()){
         case 0:
-        valuesOfGrid[verticalPosition][horizontalPosition]->setStyleSheet("QLabel{background-color: #ccc79b;}");
-        break;
+            valuesOfGrid[verticalPosition][horizontalPosition]->setStyleSheet("QLabel{background-color: #ccc79b;}");
+            break;
         case 2:
-        valuesOfGrid[verticalPosition][horizontalPosition]->setStyleSheet("QLabel{background-color: blue; color: black; font: 15pt; border-radius: 7px}");
-        break;
+            valuesOfGrid[verticalPosition][horizontalPosition]->setStyleSheet("QLabel{background-color: blue; color: black; font: 15pt; border-radius: 7px}");
+            break;
+        case 4:
+            valuesOfGrid[verticalPosition][horizontalPosition]->setStyleSheet("QLabel{background-color: red; color: black; font: 15pt; border-radius: 7px}");
+            break;
     }
 }
 
  void TwentyFortyEight:: placeRandomValueInGrid(){
      bool gridIsEmpty = true;
+     bool gridIsFull = true;
      for(int verticalIterator = 0; verticalIterator < 4; verticalIterator++)
         {
          for(int horizontalIterator = 0; horizontalIterator < 4; horizontalIterator++)
@@ -71,10 +75,41 @@ void TwentyFortyEight::changeColor(int verticalPosition, int horizontalPosition)
              }
          }
      }
+
+     for(int verticalIterator = 0; verticalIterator < 4; verticalIterator++)
+        {
+         for(int horizontalIterator = 0; horizontalIterator < 4; horizontalIterator++)
+            {
+             if(valuesOfGrid[verticalIterator][horizontalIterator]->text() == "")
+             {
+                 gridIsFull = false;
+             }
+         }
+     }
+
      if(gridIsEmpty == true){
          int randomVerticalPosition = qrand()%4;
          int randomHorizontalPosition = qrand()%4;
          valuesOfGrid[randomVerticalPosition][randomHorizontalPosition]->setText("2");
          changeColor(randomVerticalPosition, randomHorizontalPosition);
+     }
+
+     if(gridIsFull == false && gridIsEmpty == false)
+     {
+         int randomVerticalPosition = 0;
+         int randomHorizontalPosition = 0;
+         do{
+             randomVerticalPosition = qrand()%4;
+             randomHorizontalPosition = qrand()%4;
+           }while(valuesOfGrid[randomVerticalPosition][randomHorizontalPosition]->text() != "");
+         int twoOrFour = (qrand()%2+1) * 2;
+         QString eitherOr = QString::number(twoOrFour);
+         valuesOfGrid[randomVerticalPosition][randomHorizontalPosition]->setText(eitherOr);
+         changeColor(randomVerticalPosition, randomHorizontalPosition);
+     }
+
+     if(gridIsFull == true)
+     {
+         int x = 0;
      }
  }
