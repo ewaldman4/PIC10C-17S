4807e01b555f495ffcb587f6061f4e484466a26b
ewaldman4 <ewaldman4@yahoo.com>
2017-06-11 14:38:07 -0700

    Created a rudimentary grid and color function

--- Final Project/twentyFortyEight.cpp
+++ Final Project/twentyFortyEight.cpp
@@ -13,18 +13,25 @@ TwentyFortyEight::TwentyFortyEight(QWidget *parent) :
     newGame->setText("New Game");
     grid->addWidget(newGame,0,0);
 
-    score = new QLabel("Score: \n 0");
+    gameTitle = new QLabel;
+    gameTitle->setText("2048");
+    gameTitle->setAlignment(Qt::AlignCenter);
+    gameTitle->setStyleSheet("QLabel{background-color: #46dbbd; color: white; font: 20pt}");
+    grid->addWidget(gameTitle,0,1);
+
+    score = new QLabel;
+    score->setText("Score: \n 0");
     score->setAlignment(Qt::AlignCenter);
     score->setStyleSheet("QLabel{background-color: #bbaaa9; color: darkRed; font:15pt;}");
-    grid->addWidget(score,0,1);
+    grid->addWidget(score,0,2);
 
     for(int verticalIterator = 0; verticalIterator < 4; verticalIterator++)
         {
         for(int horizontalIterator = 0; horizontalIterator < 4; horizontalIterator++)
             {
-            valuesOfGrid[verticalIterator][horizontalIterator] = new QLabel("1");
-            valuesOfGrid[verticalIterator][horizontalIterator] ->setAlignment(Qt::AlignCenter);
-            valuesOfGrid[verticalIterator][horizontalIterator] ->setStyleSheet("QLabel{background-color: white; color: black; font: 15pt; border-radius: 7px}");
+            valuesOfGrid[verticalIterator][horizontalIterator] = new QLabel("");
+            valuesOfGrid[verticalIterator][horizontalIterator]->setAlignment(Qt::AlignCenter);
+            changeColor(verticalIterator, horizontalIterator);
             grid->addWidget(valuesOfGrid[verticalIterator][horizontalIterator], verticalIterator + 1, horizontalIterator);
             }
         }
@@ -35,4 +42,12 @@ TwentyFortyEight::TwentyFortyEight(QWidget *parent) :
 TwentyFortyEight::~TwentyFortyEight()
 {
     delete ui;
+}
+
+void TwentyFortyEight::changeColor(int verticalPosition, int horizontalPosition)
+{
+    switch(valuesOfGrid[verticalPosition][horizontalPosition]->text().toInt()){
+        case 0:
+        valuesOfGrid[verticalPosition][horizontalPosition]->setStyleSheet("QLabel{background-color: white; color: black; font: 15pt; border-radius: 7px}");
+    }
 }
