93de2fd01d98792df749262b9e8922a1d44e4b34
ewaldman4 <ewaldman4@yahoo.com>
2017-06-12 14:51:36 -0700

    Added an up function, ran into errors

--- Final Project/twentyFortyEight.cpp
+++ Final Project/twentyFortyEight.cpp
@@ -33,13 +33,19 @@ TwentyFortyEight::TwentyFortyEight(QWidget *parent) :
             {
             valuesOfGrid[verticalIterator][horizontalIterator] = new QLabel("");
             valuesOfGrid[verticalIterator][horizontalIterator]->setAlignment(Qt::AlignCenter);
-            changeColor(verticalIterator, horizontalIterator);
+            changeTile(verticalIterator, horizontalIterator);
             grid->addWidget(valuesOfGrid[verticalIterator][horizontalIterator], verticalIterator + 1, horizontalIterator);
             }
         }
 
     setFixedSize(400,400);
+    setFocus();
     placeRandomValueInGrid();
+    valuesOfGrid[1][0]->setText("2");
+    changeTile(1,0);
+    valuesOfGrid[2][0]->setText("2");
+    changeTile(2,0);
+
 }
 
 TwentyFortyEight::~TwentyFortyEight()
@@ -47,7 +53,7 @@ TwentyFortyEight::~TwentyFortyEight()
     delete ui;
 }
 
-void TwentyFortyEight::changeColor(int verticalPosition, int horizontalPosition)
+void TwentyFortyEight::changeTile(int verticalPosition, int horizontalPosition)
 {
     switch(valuesOfGrid[verticalPosition][horizontalPosition]->text().toInt()){
         case 0:
@@ -91,7 +97,7 @@ void TwentyFortyEight::changeColor(int verticalPosition, int horizontalPosition)
          int randomVerticalPosition = qrand()%4;
          int randomHorizontalPosition = qrand()%4;
          valuesOfGrid[randomVerticalPosition][randomHorizontalPosition]->setText("2");
-         changeColor(randomVerticalPosition, randomHorizontalPosition);
+         changeTile(randomVerticalPosition, randomHorizontalPosition);
      }
 
      if(gridIsFull == false && gridIsEmpty == false)
@@ -103,13 +109,67 @@ void TwentyFortyEight::changeColor(int verticalPosition, int horizontalPosition)
              randomHorizontalPosition = qrand()%4;
            }while(valuesOfGrid[randomVerticalPosition][randomHorizontalPosition]->text() != "");
          int twoOrFour = (qrand()%2+1) * 2;
-         QString eitherOr = QString::number(twoOrFour);
-         valuesOfGrid[randomVerticalPosition][randomHorizontalPosition]->setText(eitherOr);
-         changeColor(randomVerticalPosition, randomHorizontalPosition);
+         valuesOfGrid[randomVerticalPosition][randomHorizontalPosition]->setText(QString::number(twoOrFour));
+         changeTile(randomVerticalPosition, randomHorizontalPosition);
      }
 
      if(gridIsFull == true)
      {
          int x = 0;
      }
+ }
+
+ void TwentyFortyEight::keyPressEvent(QKeyEvent *event)
+ {
+     switch(event->key())
+     {
+         case Qt::Key_Up:
+             upIsPressed();
+             break;
+             /*
+         case Qt::Key_Left():
+             leftIsPressed();
+             break;
+         case Qt::Key_Right():
+             rightIsPressed();
+             break;
+         case Qt::Key_Down():
+             downIsPressed();
+             break;
+             */
+         default:
+             QMainWindow::keyPressEvent(event);
+     }
+     placeRandomValueInGrid();
+ }
+
+ void TwentyFortyEight::upIsPressed()
+ {
+     for(int verticalIterator = 3; verticalIterator > 0; verticalIterator--)
+        {for(int horizontalIterator = 0; horizontalIterator < 4; horizontalIterator++)
+         {
+           if(valuesOfGrid[verticalIterator][horizontalIterator]->text() == valuesOfGrid[verticalIterator--][horizontalIterator]->text()
+                   && valuesOfGrid[verticalIterator][horizontalIterator]->text() != "")
+           {
+               valuesOfGrid[verticalIterator][horizontalIterator]->setText("");
+               changeTile(verticalIterator, horizontalIterator);
+               valuesOfGrid[verticalIterator--][horizontalIterator]->setText(QString::number(2*valuesOfGrid[verticalIterator--][horizontalIterator]->text().toInt()));
+               changeTile(verticalIterator--, horizontalIterator);
+           }
+         }
+     }
+     /*
+     for(int verticalIterator = 0; verticalIterator < 3; verticalIterator++)
+        {for(int horizontalIterator = 0; horizontalIterator < 4; horizontalIterator++)
+            {
+             if(valuesOfGrid[verticalIterator][horizontalIterator]->text() == "" && valuesOfGrid[verticalIterator++][horizontalIterator]->text() != "")
+             {
+                 valuesOfGrid[verticalIterator][horizontalIterator]->setText(valuesOfGrid[verticalIterator++][horizontalIterator]->text());
+                 changeTile(verticalIterator, horizontalIterator);
+                 valuesOfGrid[verticalIterator++][horizontalIterator]->setText("");
+                 changeTile(verticalIterator++, horizontalIterator);
+             }
+            }
+     }
+     */
  }
